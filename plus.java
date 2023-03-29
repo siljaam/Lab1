0803e069f8b3738d467ad3e8c417eeca014fae27
VilleLahtinen <91072389+kalustaja2k@users.noreply.github.com>
2023-03-29 15:32:02 +0300

    update plus.java
    added multiplication

--- plus.java
+++ plus.java
@@ -1,11 +1,17 @@
 public class IntegerSubtractionExample1 {  
     public static void main(String[] args) {          
-      int a = 65;  
+          
+        int a = 65;  
         int b = 35; 
       
         int subtraction = a-b;
+        int multiplication = a*b;
+          
       
-        System.out.println("The subtraction of a and b is = " + subtraction);  
+        System.out.println("The subtraction of a and b is = " + subtraction);
+        System.out.println("The multiplication of a and b is = " + multiplication);   
+      
+         
     }  
 }  
  
