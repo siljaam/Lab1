c9de5598db4806d82391e83db701eb1ba124284e
VilleLahtinen <91072389+kalustaja2k@users.noreply.github.com>
2023-03-29 15:36:13 +0300

    Create multi.java
    Added a multiplication code

---
+++ multi.java
@@ -0,0 +1,14 @@
+public class IntegerMultiplicationExample1 {  
+    public static void main(String[] args) {          
+          
+        int a = 65;  
+        int b = 35; 
+      
+        int multiplication = a*b;
+          
+        System.out.println("The multiplication of a and b is = " + multiplication);   
+      
+         
+    }  
+}  
+ 
